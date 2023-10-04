f07ab89b13fb7764b15191ee412d1d4a02f0c7b1
vikas singh <pokhriyalvikas940@gmail.com>
2023-10-04 16:26:53 +0530

    four commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const textarea = document.getElementById('type-box');
 const char_element = document.getElementById('char');
 const twitter_element = document.getElementById('twitter-number');
 const insta_element = document.getElementById('insta-number');
+const word_element = document.getElementById('word');
+
 
 
 let x_char_limit = 280;
@@ -9,9 +11,30 @@ let insta_char_limit = 160;
 
 
 
+
 const charCounter = (e) => {
     char_element.innerHTML = e.target.value.length;
 }
+
+let space_length_number = 0;
+
+const wordCounter = (e) => {
+    let str = e.target.value;
+    str = str.trim();
+
+    if (str==='') {
+        space_length_number = 0;
+    } else if (str.indexOf(' ')=== -1) {
+        space_length_number = 1;
+    } else {
+        const words = str.split(/\s+/);
+        space_length_number = words.length;
+    }
+    word_element.innerText = space_length_number;
+}
+
+
+
 
 const twitterCounter = (e) => {
     twitter_element.innerHTML = x_char_limit - e.target.value.length;
@@ -44,5 +67,6 @@ textarea.addEventListener('input', (e) => {
     charCounter(e);
     twitterCounter(e);
     instaCounter(e);
+    wordCounter(e);
 
 })
